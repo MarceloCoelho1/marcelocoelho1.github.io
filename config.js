a37faf4850e585a4915b3db9a40702beafd5db47
Marcelo <mhbcoelho99@gmail.com>
2023-11-26 08:22:45 -0300

    adding instagram

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Instagram',
+			icon: 'instagram',
+			link: 'https://www.instagram.com/marcelo_henri22/',
 		},
 		{
 			id: '4',
